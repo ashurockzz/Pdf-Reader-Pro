3a177c3f6d1e9eae569b40fbb4b59c3ed60596b8
ashurockzz <patnaashutosh@gmail.com>
2024-07-05 01:48:36 +0530

    commit to change branch

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,35 +1,45 @@
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-import { publicProcedure, router } from './trpc'
-import { TRPCError } from '@trpc/server'
-import { db } from '@/db'
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { TRPCError } from "@trpc/server";
+import { db } from "@/db";
+import { privateProcedure, publicProcedure, router } from "./trpc";
 
 export const appRouter = router({
-    authCallback: publicProcedure.query(async() => {
-        const { getUser } = getKindeServerSession()
-        const user = getUser()
+  authCallback: publicProcedure.query(async () => {
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
 
-        if(!user.id || !user.email)
-            throw new TRPCError({ code: 'UNAUTHORIZED' })
+    if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
 
-        //check if the user in the database
-        const dbUser = await db.user.findFirst({
-            where: {
-              id: user.id,
-            },
-          })
+    // check if the user is in the database
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    });
 
-          if (!dbUser) {
-            // create user in db
-            await db.user.create({
-              data: {
-                id: user.id,
-                email: user.email,
-              },
-            })
-          }
+    if (!dbUser) {
+      // create user in db
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      });
+    }
 
-        return { success: true }
+    return { success: true };
+  }),
+
+  getUserFiles: privateProcedure.query(async({ctx}) => {
+    const { userId } = ctx
+    
+    return await db.file.findMany({
+      where: {
+        userId
+      }
     })
-})
+  })
+});
 
-export type Approuter = typeof appRouter
+export type AppRouter = typeof appRouter;
+
