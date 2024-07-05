db151092121ab79d18e32381806c55db6fff893f
ashurockzz <patnaashutosh@gmail.com>
2024-07-06 01:35:51 +0530

    added deleteFile functionality

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -2,6 +2,7 @@ import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
 import { privateProcedure, publicProcedure, router } from "./trpc";
+import { z } from 'zod'
 
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
@@ -38,7 +39,24 @@ export const appRouter = router({
         userId
       }
     })
-  })
+  }),
+  deleteFile: privateProcedure.input(z.object({id: z.string()})).mutation(async ({ctx, input}) => {
+    const {userId} = ctx
+    const file = await db.file.findFirst({
+      where: {
+        id: input.id,
+        userId,
+      }
+    })
+    if(!file) throw new TRPCError({ code: 'NOT_FOUND'})
+
+    await db.file.delete({
+      where:{
+        id: input.id,
+      }
+    })
+    return file
+  }),
 });
 
 export type AppRouter = typeof appRouter;
